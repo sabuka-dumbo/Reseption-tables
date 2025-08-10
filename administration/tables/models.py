e269d5bd360bc16eaadfa537e80ef237f9fdb14b
Sabuka-Dumbo <anonym.sas@proton.me>
2025-08-10 12:22:04 +0000

    Aug 10, 2025, 4:22 PM

--- administration/tables/models.py
+++ administration/tables/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+import datetime
 
 # Create your models here.
+class Room(models.Model):
+    room_number = models.CharField(max_length=10)
+    check_in = models.DateTimeField(default=datetime.datetime.now)
+    check_out = models.DateTimeField(default=datetime.datetime.now)
+    guest_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"Room {self.room_number} ({self.guest_name})"
