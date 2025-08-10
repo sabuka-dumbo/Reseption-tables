738cc0e7f5c58bd8482ef5e015694cc81fbd23a5
Sabuka-Dumbo <anonym.sas@proton.me>
2025-08-10 12:51:51 +0000

    Aug 10, 2025, 4:51 PM

--- administration/tables/templatetags/custom_filters.py
+++ administration/tables/templatetags/custom_filters.py
@@ -3,15 +3,12 @@ from django import template
 register = template.Library()
 
 @register.filter
-def get_item(dictionary, key):
-    return dictionary.get(key, '')
-
-register = template.Library()
-
-@register.filter
 def get_booking_for_day(booking_groups, day):
-    """Find which booking group a specific day belongs to"""
     for booking in booking_groups:
         if booking['start'] <= day <= booking['end']:
             return booking
     return None
+
+@register.filter
+def get_item(dictionary, key):
+    return dictionary.get(key, '')
