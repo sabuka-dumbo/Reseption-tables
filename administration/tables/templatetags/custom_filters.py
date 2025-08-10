b048a756c13e851583faf33efeb7de2418ec0c70
Sabuka-Dumbo <anonym.sas@proton.me>
2025-08-10 12:49:18 +0000

    Aug 10, 2025, 4:49 PM

--- administration/tables/templatetags/custom_filters.py
+++ administration/tables/templatetags/custom_filters.py
@@ -5,3 +5,13 @@ register = template.Library()
 @register.filter
 def get_item(dictionary, key):
     return dictionary.get(key, '')
+
+register = template.Library()
+
+@register.filter
+def get_booking_for_day(booking_groups, day):
+    """Find which booking group a specific day belongs to"""
+    for booking in booking_groups:
+        if booking['start'] <= day <= booking['end']:
+            return booking
+    return None
